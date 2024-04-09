b54359e75d4c0e72f8fbc083655aa40b115a51f8
devhimanshuu <devhimanshuu@gmail.com>
2024-04-10 03:47:32 +0530

    App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
+function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  return <div className="App">app {url?.total_pages}</div>;
+}
+
+export default App;
