4d4e847ccaa9fd3114dca300b433c1350c9bf423
devhimanshuu <devhimanshuu@gmail.com>
2024-04-22 02:33:00 +0530

    pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./style.scss";
-
+import Popular from "./Popular/Popular";
 import HeroBanner from "./heroBanner/HeroBanner";
+import TopRated from "./TopRated/TopRated";
 import Upcoming from "./Upcoming/Upcoming";
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Upcoming />
-      <div style={{ height: 1000 }}></div>
+      <Popular />
+      <TopRated />
     </div>
   );
 };
