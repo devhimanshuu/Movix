9f66a70157530586e0ac77a5343711dca2555874
devhimanshuu <devhimanshuu@gmail.com>
2026-02-05 12:56:29 +0530

    feat: Implement media details page with banner, cast, videos, and recommendations, and add a global footer component.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,51 +7,51 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import "./style.scss";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <ContentWrapper>
-        <ul className="menuItems">
-          <li className="menuItem">Terms Of Use</li>
-          <li className="menuItem">Privacy-Policy</li>
-          <li className="menuItem">About</li>
-          <li className="menuItem">Blog</li>
-          <li className="menuItem">FAQ</li>
-        </ul>
-        <div className="infoText">
-          At Movix, we're dedicated to bringing you the best in movies. Explore
-          our vast collection of films, TV shows, and documentaries. Join our
-          community of movie lovers to share recommendations and engage in
-          lively discussions. Welcome to Movix – Where Every Screen Tells a
-          Story.
-        </div>
-        <div className="socialIcons">
-          <span className="icon">
-            <a href="https://github.com/devhimanshuu">
-              <FaGithub />
-            </a>
-          </span>
-          <span className="icon">
-            <a href="https://techsphere.hashnode.dev/">
-              <FaHashnode />
-            </a>
-          </span>
-          <span className="icon">
-            <a href="https://twitter.com/devhimanshuu">
-              <FaTwitter />
-            </a>
-          </span>
-          <span className="icon">
-            <a href="https://www.linkedin.com/in/himanshu-guptaa/">
-              <FaLinkedin />
-            </a>
-          </span>
-        </div>
-        <div className="endName">
-          <p>Created By Himanshu Gupta</p>
-        </div>
-      </ContentWrapper>
-    </footer>
-  );
+	return (
+		<footer className="footer">
+			<ContentWrapper>
+				<ul className="menuItems">
+					<li className="menuItem">Terms Of Use</li>
+					<li className="menuItem">Privacy-Policy</li>
+					<li className="menuItem">About</li>
+					<li className="menuItem">Blog</li>
+					<li className="menuItem">FAQ</li>
+				</ul>
+				<div className="infoText">
+					At Movix, we're dedicated to bringing you the best in movies. Explore
+					our vast collection of films, TV shows, and documentaries. Join our
+					community of movie lovers to share recommendations and engage in
+					lively discussions. Welcome to Movix – Where Every Screen Tells a
+					Story.
+				</div>
+				<div className="socialIcons">
+					<span className="icon">
+						<a href="https://github.com/devhimanshuu">
+							<FaGithub />
+						</a>
+					</span>
+					<span className="icon">
+						<a href="https://techsphere.hashnode.dev/">
+							<FaHashnode />
+						</a>
+					</span>
+					<span className="icon">
+						<a href="https://twitter.com/devhimanshuu">
+							<FaTwitter />
+						</a>
+					</span>
+					<span className="icon">
+						<a href="https://www.linkedin.com/in/himanshu-guptaa/">
+							<FaLinkedin />
+						</a>
+					</span>
+				</div>
+				<div className="endName">
+					<div className="signature-wrapper">Created By Himanshu Gupta</div>
+				</div>
+			</ContentWrapper>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -12,55 +12,55 @@ import Reviews from "./Reviews/Reviews";
 import { addToHistory } from "../../store/historySlice";
 
 const Details = () => {
-  const { mediaType, id } = useParams();
-  const dispatch = useDispatch();
-  const { url } = useSelector((state) => state.home);
-  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data: credits, loading: creditsLoading } = useFetch(
-    `/${mediaType}/${id}/credits`,
-  );
-  const detailsData = useFetch(`/${mediaType}/${id}`).data;
-  const { data: reviews, loading: reviewsLoading } = useFetch(
-    `/${mediaType}/${id}/reviews`,
-  );
+	const { mediaType, id } = useParams();
+	const dispatch = useDispatch();
+	const { url } = useSelector((state) => state.home);
+	const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+	const { data: credits, loading: creditsLoading } = useFetch(
+		`/${mediaType}/${id}/credits`,
+	);
+	const detailsData = useFetch(`/${mediaType}/${id}`).data;
+	const { data: reviews, loading: reviewsLoading } = useFetch(
+		`/${mediaType}/${id}/reviews`,
+	);
 
-  // Track view in history when details page loads
-  useEffect(() => {
-    if (detailsData?.id) {
-      // Construct the full poster URL while we have access to url
-      const posterUrl = detailsData.poster_path
-        ? url?.poster + detailsData.poster_path
-        : null;
+	// Track view in history when details page loads
+	useEffect(() => {
+		if (detailsData?.id) {
+			// Construct the full poster URL while we have access to url
+			const posterUrl = detailsData.poster_path
+				? url?.poster + detailsData.poster_path
+				: null;
 
-      dispatch(
-        addToHistory({
-          id: detailsData.id,
-          title: detailsData.title || detailsData.name,
-          name: detailsData.name,
-          poster_path: detailsData.poster_path,
-          posterUrl: posterUrl, // Store full URL for easy access
-          media_type: mediaType,
-          vote_average: detailsData.vote_average || 0,
-          release_date: detailsData.release_date || detailsData.first_air_date,
-          first_air_date: detailsData.first_air_date,
-          genre_ids: detailsData.genres?.map((g) => g.id) || [],
-          genres: detailsData.genres || [],
-          backdrop_path: detailsData.backdrop_path,
-        }),
-      );
-    }
-  }, [detailsData?.id, mediaType, dispatch, url]);
+			dispatch(
+				addToHistory({
+					id: detailsData.id,
+					title: detailsData.title || detailsData.name,
+					name: detailsData.name,
+					poster_path: detailsData.poster_path,
+					posterUrl: posterUrl, // Store full URL for easy access
+					media_type: mediaType,
+					vote_average: detailsData.vote_average || 0,
+					release_date: detailsData.release_date || detailsData.first_air_date,
+					first_air_date: detailsData.first_air_date,
+					genre_ids: detailsData.genres?.map((g) => g.id) || [],
+					genres: detailsData.genres || [],
+					backdrop_path: detailsData.backdrop_path,
+				}),
+			);
+		}
+	}, [detailsData?.id, mediaType, dispatch, url]);
 
-  return (
-    <div>
-      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
-      <Cast data={credits?.cast} loading={creditsLoading} />
-      <VideosSection data={data} loading={loading} />
-      <Reviews data={reviews} loading={reviewsLoading} />
-      <Similar mediaType={mediaType} id={id} />
-      <Recommendation mediaType={mediaType} id={id} />
-    </div>
-  );
+	return (
+		<div>
+			<DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+			<Cast data={credits?.cast} loading={creditsLoading} />
+			<VideosSection data={data} loading={loading} />
+			<Reviews data={reviews} loading={reviewsLoading} />
+			<Similar mediaType={mediaType} id={id} />
+			<Recommendation mediaType={mediaType} id={id} />
+		</div>
+	);
 };
 
 export default Details;
